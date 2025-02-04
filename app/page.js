0afe0495d7e7db8ca30f530971f788f3b07ca653
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2025-02-03 18:27:40 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Intro from "./components/intro";
+//import Intro from "./components/intro";
 import Portfolio from "./components/Portfolio";
 import Timeline from "./components/Timeline";
 // import Footer from "./components/Footer";
@@ -11,7 +11,6 @@ export default function Home() {
       <div className="absolute top-4 right-4">
         <ThemeToggle />
       </div>
-      <Intro />
       <Portfolio />
       <Timeline />
     </main>
