67af203ee658685472a06c956fbf79cbbff487b5
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2025-02-03 18:20:43 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Intro from "./components/Intro";
+import Intro from "./components/intro";
 import Portfolio from "./components/Portfolio";
 import Timeline from "./components/Timeline";
 // import Footer from "./components/Footer";
