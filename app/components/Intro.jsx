54221365615ab9aa089610a488951baea94d9638
AsenaiShiberim3 <130591219+AsenaiShiberim@users.noreply.github.com>
2025-02-27 11:29:55 -0700

    Theme
    Reset to default theme

--- app/components/Intro.jsx
+++ app/components/Intro.jsx
@@ -5,10 +5,10 @@ export default function Intro() {
       <h1 className="text-2xl md:text-7xl mb-2 md:mb-4 font-bold">
         Asenai Shiberim
       </h1>
-      <p className="text-base md:text-xl mb-4 font-medium text-gray-600 dark:text-gray-300">
+      <p className="text-base md:text-xl mb-4 font-medium text-gray-600 ">
         Software Developer
       </p>
-      <p className="text-sm md:text-base max-w-3xl mb-6 font-medium text-gray-800 dark:text-gray-200">
+      <p className="text-sm md:text-base max-w-3xl mb-6 font-medium text-gray-800 ">
       I am a recent graduate from the Southern Alberta Institute of Technology with a diploma in Software Development. My passion for music production sparked my interest in technology, driving me to explore how the two fields can intersect. I have experience with Java, JavaScript, and Python, and I am currently in the process of learning C++ to dive deeper into audio engineering.
       </p>
     </div>
--- app/page.js
+++ app/page.js
@@ -2,14 +2,14 @@ import Intro from "./components/Intro";
 import Portfolio from "./components/Portfolio";
 import Timeline from "./components/Timeline";
 // import Footer from "./components/Footer";
-import { ThemeToggle } from "./components/ThemeToggle";
+//import { ThemeToggle } from "./components/ThemeToggle";
 
 export default function Home() {
   return (
-    <main className="dark:bg-dark relative min-h-screen">
+    <main className=" relative min-h-screen">
       {/* Move ThemeToggle to the right */}
       <div className="absolute top-4 right-4">
-        <ThemeToggle />
+        
       </div>
       <Intro />
       <Portfolio />
